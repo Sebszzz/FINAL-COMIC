78e2f0e4eeddb14bd128264989abba28a50d1390
Sebi <124313209+Sebszzz@users.noreply.github.com>
2023-04-19 23:12:41 -0400

    Add files via upload

---
+++ COMIC/js/COMIC.java
@@ -0,0 +1,21 @@
+let popupShown = false;
+
+function showPopup() {
+  document.getElementById("popup1").style.display = "block";
+  popupShown = true;
+}
+
+function chooseOption(option) {
+  if (option === "option1") {
+    alert("You chose option 1");
+  } else if (option === "option2") {
+    alert("You chose option 2");
+  }
+  document.getElementById("popup1").style.display = "none";
+}
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 500 && !popupShown) {
+    showPopup();
+  }
+});
