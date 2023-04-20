a92c3480f2ca1d95fc89bddeecb9f95dc82e5762
Sebi <124313209+Sebszzz@users.noreply.github.com>
2023-04-20 15:47:34 -0400

    Add files via upload

---
+++ COMIC/COMIC/COMIC.java
@@ -0,0 +1,12 @@
+function selectOption(option) {
+  // Get the question box and container element corresponding to the selected option
+  var questionBox = document.getElementById('question-box' + option.charAt(option.length-1));
+  var container = document.getElementById('container' + option.charAt(option.length-1));
+
+  // Get the GIF element and show it
+  var gif = container.querySelector('oh.gif');
+  gif.style.display = 'block';
+
+  // Hide the question box
+  questionBox.style.display = 'none';
+}
